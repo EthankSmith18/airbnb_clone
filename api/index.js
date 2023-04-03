35c875f87ca68cdab01d02babb22ff3a2814ef6f
Ethan Smith <EthanKSmith@gmail.com>
2023-04-03 16:27:58 -0700

    Day 1 Airbnb Clone

---
+++ api/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("./models/User");
+require("dotenv").config();
+const app = express();
+
+// Es2uUowzWljVG9Ll MongoDB pass
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = "dsfdsfsdgdfhgfhgfhfg";
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URL);
+
+app.get("/test", (req, res) => {
+  res.json("test ok");
+});
+
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userDoc = await User.findOne({ email });
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign(
+        { email: userDoc.email, id: userDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json("pass ok");
+        }
+      );
+    } else {
+      res.status(422).json("pass no ok");
+    }
+  } else {
+    res.json("User not found");
+  }
+});
+
+app.listen(4000);
